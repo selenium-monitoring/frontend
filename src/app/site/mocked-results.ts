1b69ad8a93134716a858feb832dffeb0001a921e
ProfRoxas <kuroda.taiki@gmail.com>
2023-12-18 01:58:11 +0100

    removed results

--- src/app/site/mocked-results.ts
+++
@@ -1,7 +0,0 @@
-import { Result } from "./result.model";
-
-export const results = [
-    new Result(0, 'First Result', new Date(), 'Success'),
-    new Result(1, 'First Result', new Date(), 'Error'),
-    new Result(2, 'First Result', new Date(), 'Success'),
-  ]
